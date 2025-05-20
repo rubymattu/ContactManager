c2b9181727c4960360d0c1a3f11949ca0af5c957
Raveena Mattu <Raveena.Mattu@triosstudent.com>
2025-05-20 10:43:33 -0400

    Added header and footer

--- index.php
+++ index.php
@@ -3,9 +3,20 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Contact Manager</title>
+  <title>Contact Manager - Home</title>
+  <link rel="stylesheet" type="txt/css" href="css/main.css">
 </head>
 <body>
-  
+  <?php
+  // Include the header file
+  include 'header.php';
+  ?>
+  <main>
+    <h2>Contact List</h2>
+  </main>
+  <?php
+  // Include the footer file
+  include 'footer.php';
+  ?>
 </body>
 </html>
