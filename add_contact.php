6b31b45ad51b43a4cd6f132d35aeb02a02c67e61
Raveena Mattu <Raveena.Mattu@triosstudent.com>
2025-05-26 10:44:52 -0400

    Added Validation

--- add_contact.php
+++ add_contact.php
@@ -18,20 +18,47 @@
   // $dob = $_POST['dob'];
 
   require_once('database.php');
-  //insert data into the database
-  $query = 'INSERT INTO contacts
-               (firstName, lastName, emailAddress, phoneNumber, status, dob)
-            VALUES
-               (:firstName, :lastName, :emailAddress, :phoneNumber, :status, :dob)';
-  $statement = $db->prepare($query);
-  $statement->bindValue(':firstName', $firstName);
-  $statement->bindValue(':lastName', $lastName);
-  $statement->bindValue(':emailAddress', $emailAddress);
-  $statement->bindValue(':phoneNumber', $phoneNumber);
-  $statement->bindValue(':status', $status);
-  $statement->bindValue(':dob', $dob);
-  $statement->execute();
-  $statement->closeCursor();
+
+  $queryContacts = 'SELECT * FROM contacts';
+  $statement1 = $db->prepare($queryContacts);
+  $statement1->execute();
+  $contacts = $statement1->fetchAll();
+  $statement1->closeCursor();
+
+  foreach ($contacts as $contact) {
+      if ($contact['emailAddress'] == $emailAddress) {
+          $_SESSION['error'] = 'Email address already exists.';
+          header('Location: error.php');
+          die();
+      }
+  }
+  //validate the data
+  if ($firstName == null || $lastName == null || $emailAddress == null || 
+      $phoneNumber == null || $status == null || $dob == null) {
+      //set an error message in the session
+      session_start();
+      //redirect to the error page
+      $_SESSION['error'] = 'Please fill in all required fields.';
+      header('Location: error.php');
+      die();
+  } else {
+          //insert data into the database
+      $query = 'INSERT INTO contacts
+                  (firstName, lastName, emailAddress, phoneNumber, status, dob)
+                VALUES
+                  (:firstName, :lastName, :emailAddress, :phoneNumber, :status, :dob)';
+      $statement = $db->prepare($query);
+      $statement->bindValue(':firstName', $firstName);
+      $statement->bindValue(':lastName', $lastName);
+      $statement->bindValue(':emailAddress', $emailAddress);
+      $statement->bindValue(':phoneNumber', $phoneNumber);
+      $statement->bindValue(':status', $status);
+      $statement->bindValue(':dob', $dob);
+      $statement->execute();
+      $statement->closeCursor();
+  }
+
+
   
   $_SESSION['fullName'] = $firstName . ' ' . $lastName;
   //redirect to the confirmation page
